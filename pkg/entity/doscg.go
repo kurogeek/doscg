f6a91a60e97d478ceed8bc7a72b6bdb74a8dde30
phanupong <phanupong.janathapoon@rdtronic.com>
2020-05-14 17:02:38 +0700

    add json notation

--- pkg/entity/doscg.go
+++ pkg/entity/doscg.go
@@ -1,30 +1,30 @@
 package entity
 
 type XYZ struct {
-	X int
-	Y int
-	Z int
+	X int `json:"x"`
+	Y int `json:"y"`
+	Z int `json:"z"`
 }
 
 type BC struct {
-	B int
-	C int
+	B int `json:"b"`
+	C int `json:"c"`
 }
 
 type LatLng struct {
-	Lat float64
-	Lng float64
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
 }
 
 type Place struct {
-	Name     string
-	Location LatLng
+	Name     string `json:"name"`
+	Location LatLng `json:"location"`
 }
 
 type BestRoute struct {
-	OriginLocation      Place
-	DestinationLocation Place
-	Polyline            []LatLng
+	OriginLocation      Place    `json:"originLocation"`
+	DestinationLocation Place    `json:"destinationLocation"`
+	Polyline            []LatLng `json:"polyline"`
 }
 
 type BotMessage struct {
