b231400404fc33fcf1745d25cd68c14fff81543b
phanupong <phanupong.janathapoon@rdtronic.com>
2020-05-14 17:10:56 +0700

    add some handlers

--- pkg/doscg/handler/echo.go
+++ pkg/doscg/handler/echo.go
@@ -11,5 +11,34 @@ type DoSCGHandler struct {
 }
 
 func NewDoSCGHandler(e *echo.Echo, du doscg.DoSCG) {
+	handler := &DoSCGHandler{
+		SCGService: du,
+	}
+	e.GET("/find-xyz", handler.FindXYZ)
+}
 
+func (dh DoSCGHandler) FindXYZ(c echo.Context) error {
+
+	xyz := dh.SCGService.FindXYZ()
+
+	return c.JSON(200, ResposeMessage{
+		Data: &DataMessage{
+			Type:       "xyz",
+			Attributes: xyz,
+		},
+	})
+}
+
+func (dh DoSCGHandler) FindBC(c echo.Context) error {
+
+	ans1 := 23
+	ans2 := -21
+	bc := dh.SCGService.FindBC(ans1, ans2)
+
+	return c.JSON(200, ResposeMessage{
+		Data: &DataMessage{
+			Type:       "bc",
+			Attributes: bc,
+		},
+	})
 }
